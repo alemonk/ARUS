0866dc02cf98343c93975f45b0a97f7e70b1f763
alemonk <alecappellari00@gmail.com>
2024-05-23 12:07:33 +0200

    example of unet

--- install.sh
+++ install.sh
@@ -5,3 +5,8 @@ source myenv/bin/activate
 
 pip3 install opencv-python
 pip3 install matplotlib
+pip3 install keras
+pip3 install tensorflow
+pip3 install torch torchvision
+pip3 install -U scikit-learn
+pip3 install segmentation_models_pytorch
